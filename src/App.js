0888df29bfdbde4dfd5826a5d9f30661fc214953
saihyny <satyasaikiranrocks@gmail.com>
2023-11-03 20:16:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React,{useState} from 'react';
-
-import MoviesList from './components/MoviesList';
-import './App.css';
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [movies,setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
- 
-  async function FetchMoviesHan () {
-    setLoading(true)
-  const response =  await fetch('https://jsonplaceholder.typicode.com/albums');
-  const data =  await response.json();
-     setLoading(false)
-      const MoviesList = data.map((moviesdata)=>{
+  const [error, setError] = useState(null);
+  const [isCanceling, setIsCanceling] = useState(false);
+  async function FetchMoviesHan() {
+    if (isCanceling) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/album"
+      );
+      if (response.status === 404) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+      const data = await response.json();
+      setLoading(false);
+      const MoviesList = data.map((moviesdata) => {
         return {
-          id:moviesdata.id,
-          title:moviesdata.title,
+          id: moviesdata.id,
+          title: moviesdata.title,
         };
-      })
-      setMovies(MoviesList)
+      });
+      setMovies(MoviesList);
+    } catch (error) {
+      setError(error.message);
+    }
   }
-
+  const handleCancel=()=> {
+    setIsCanceling(true);
+    
+  }
 
   return (
     <React.Fragment>
@@ -28,7 +43,15 @@ function App() {
         <button onClick={FetchMoviesHan}>Fetch Movies</button>
       </section>
       <section>
-        {loading && <h2>Movies are loading please wait for some time</h2>}
+        {loading && !error && (
+          <h2>Movies are loading please wait for some time</h2>
+        )}
+        {loading && error && (
+          <>
+            <p>{error}</p>
+            <button onClick={handleCancel}>cancel</button>
+          </>
+        )}
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
